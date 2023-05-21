89ad806663692037e86a4107e9330b34ba756cc7
Pa-Nika <verun2001@mail.ru>
2023-05-22 01:22:18 +0700

    [MAIN-T-13]: Dockerfile

--- src/main/java/ru/nsu/calendar/repository/WeekDaysNotificationRepository.java
+++ src/main/java/ru/nsu/calendar/repository/WeekDaysNotificationRepository.java
@@ -1,8 +1,10 @@
 package ru.nsu.calendar.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import ru.nsu.calendar.entities.WeekDaysNotification;
 import ru.nsu.calendar.entities.WeekDaysNotificationId;
 
+@Repository
 public interface WeekDaysNotificationRepository extends CrudRepository<WeekDaysNotification, WeekDaysNotificationId> {
 }
